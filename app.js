e1f4f0996348079a808efa6138057074f2c8c7ac
Teapotka <kalyvanivan33@gmail.com>
2023-09-11 16:20:25 +0200

    add sessions, change endpoints

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ import dotenv from "dotenv"
 import { MongoClient, ServerApiVersion } from "mongodb"
 import cors from "cors"
 
-const app = express()
-app.use(cors({ origin: '*' }));
 dotenv.config()
+
+const app = express()
 const PORT = process.env.PORT || 8080
+const DB_NAME = "kiss"
+const COLLECTION_NAME = "values"
+
 const client = new MongoClient(
   "mongodb+srv://teapotka:polina@cluster0.4wdwges.mongodb.net/?retryWrites=true&w=majority"
 )
@@ -17,48 +20,94 @@ const client = new MongoClient(
     console.log("MONGODB ERRORED " + e.message)
   })
 
-const dbName = "kiss"
-const collectionName = "values"
+async function connectToDatabase() {
+  try {
+    await client.connect()
+    console.log("Connected to the database")
+    return client.db(DB_NAME)
+  } catch (error) {
+    console.error("Database connection error: ", error)
+    throw error
+  }
+}
+
+app.use(async (req, res, next) => {
+  req.db = await connectToDatabase()
+  next()
+})
+
+app.use(cors({ origin: "*" }))
+app.use(express.json())
 
 app.get("/", async (req, res) => {
   res.send("kiss u")
 })
-app.get("/elie", async (req, res) => {
-  await client.connect()
-  const db = client.db(dbName)
-  const document = await db.collection(collectionName).findOne()
-  const previous = Number.parseInt(document.elie)
-  const result = await db.collection(collectionName).updateOne(
-    {},
-    {
-      $set: { elie: previous + 1 },
-    }
-  )
-  client.close()
-  res.json(result)
-})
-
-app.get("/timi", async (req, res) => {
-    await client.connect()
-    const db = client.db(dbName)
-    const document = await db.collection(collectionName).findOne()
-    const previous = Number.parseInt(document.timi)
-    const result = await db.collection(collectionName).updateOne(
-      {},
-      {
-        $set: { timi: previous + 1 },
-      }
-    )
-    client.close()
-    res.json(result)
-  })
 
 app.get("/values", async (req, res) => {
-    await client.connect()
-    const db = client.db(dbName)
-    const {elie, timi} = await db.collection(collectionName).findOne()
-    client.close()
-    res.json({elie, timi})
+  try {
+    const { db, query } = req
+    const { name } = query
+
+    if (!name) {
+      return res.status(400).json({ error: "Name is required" })
+    }
+
+    const session = client.startSession()
+    session.startTransaction()
+    try {
+      const collection = db.collection(COLLECTION_NAME)
+      const document = await collection.findOne({name: name},{ session })
+
+      await session.commitTransaction()
+      session.endSession()
+      res.json(document)
+    } catch (error) {
+      session.abortTransaction()
+      session.endSession()
+      console.error("Error in /values route:", error)
+      res.status(500).json({ error: "Session Error" })
+    }
+  } catch (error) {
+    console.error("Error in /values route:", error)
+    res.status(500).json({ error: "Internal Server Error" })
+  }
+})
+
+app.post("/kiss", async (req, res) => {
+  try {
+    const { body, db } = req
+    const { name } = body
+    if (!name) {
+      return res.status(400).json({ error: "Name is required" })
+    }
+
+    const session = client.startSession()
+    session.startTransaction()
+
+    try {
+      const collection = db.collection(COLLECTION_NAME)
+      const result = await collection.updateOne(
+        { name: name },
+        {
+          $inc: { value: 1 },
+        },
+        { session }
+      )
+      const document = await collection.findOne({ name: name }, { session })
+      await session.commitTransaction()
+      session.endSession()
+
+      res.json({ result, document })
+    } catch (error) {
+      session.abortTransaction()
+      session.endSession()
+      console.error("Error in /kiss route:", error)
+      res.status(500).json({ error: "Session Error" })
+    }
+  } catch (error) {
+    console.error("Error in /kiss route:", error)
+    res.status(500).json({ error: "Internal Server Error" })
+  }
 })
 
 app.listen(PORT, () => {
